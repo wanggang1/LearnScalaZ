414c5b79fd877f904bd57ecc4f7c4dd9d86d72d9
Gang Wang <gang.wang.contractor@weather.com>
2016-01-29 15:44:24 -0500

    refactoring

--- src/main/scala/org/gwgs/learningscalaz/day02/Applicative.scala
+++ src/main/scala/org/gwgs/learningscalaz/day02/Applicative.scala
@@ -22,8 +22,8 @@ object ApplicativeOps {
   def demo = {
     println("============== Applicative ===============")
                                                  
-    //println("scalaz Allpy.lift2 - concatInOption(3.some, List(4, 5).some): " + concatInOption(3.some, List(4, 5).some) )
-    //println("scalaz Allpy.lift2 - concatInOption(none[Int], List(4, 5).some): " + concatInOption(none[Int], List(4, 5).some) ) 
+    println("scalaz Allpy.lift2 - concatInOption(3.some, List(4, 5).some): " + concatInOption(3.some, List(4, 5).some) )
+    println("scalaz Allpy.lift2 - concatInOption(none[Int], List(4, 5).some): " + concatInOption(none[Int], List(4, 5).some) ) 
         
     println("Custom sequence - sequenceA(List(1.some, 2.some)): " + sequenceA(List(1.some, 2.some)) )
     println("Custom sequence - sequenceA(List(1.some, none, 2.some)): " + sequenceA(List(1.some, none, 2.some)) )
