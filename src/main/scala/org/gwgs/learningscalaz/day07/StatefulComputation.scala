608712dc68a9615ad68152322a3ef2358aed6ad3
Gang Wang <gwang@viridityenergy.com>
2016-01-30 09:45:54 -0500

    completed day 7 - State monad

--- src/main/scala/org/gwgs/learningscalaz/day07/StatefulComputation.scala
+++ src/main/scala/org/gwgs/learningscalaz/day07/StatefulComputation.scala
@@ -1,49 +1,64 @@
 package org.gwgs.learningscalaz.day07
 
+/**
+ * State monad encapsulate a function that returns a tuple
+ */
 object StatefulComputation {
   import scalaz._
   import Scalaz._
   
   type Stack = List[Int]
-  
-  def pop = State[List[Int], Int] {
-    case Nil => 
-      println("pop from Nil")
-      (List.empty[Int], -1)
-    case x :: xs => 
-      println(s"pop: $x")
-      (xs, x)
+
+  def pop = State[Stack, Int] {
+    case x :: xs => (xs, x)
   }
   
-  def push(a: Int)= State[List[Int], Unit] {
-    case Nil =>
-      println("push to Nil")
-      (a :: Nil, ())
-    case xs =>
-      println(s"push: $a")
-      (a :: xs, ())
+  def push(a: Int) = State[Stack, Unit] {
+    case xs => (a :: xs, ())
   }
+
+  /**  implement pop and push with functions on StateFunctions
+   * 
+   * val pop: State[Stack, Int] = for {
+         s <- get[Stack]
+         val (x :: xs) = s
+         _ <- put(xs)
+       } yield x
+       
+     def push(x: Int): State[Stack, Unit] = for {
+         xs <- get[Stack]
+         r <- put(x :: xs)
+       } yield r
+   */
   
-  def show(a: Int)= State[List[Int], Unit] {
-    case xs => 
-      println(s"Show Stateful: $xs and $a")
-      (xs, ())
-  }
-  
-  def stackManip(stack: Stack): State[List[Int], Int] = for {
+  /**
+   * How the existing state being passed to stackManip???
+   * see scalaz.StateFunctions
+   */
+  def stackManip: State[Stack, Int] = for {
     _ <- push(3)
     a <- pop
     b <- pop
   } yield (b)
   
+  /**
+   * get: State[S,S].  for comprehension on State getting the 2nd value of tuple2
+   */
+  def stackyStack: State[Stack, Unit] = for {
+     stackNow <- get
+     r <- if (stackNow === List(1, 2, 3)) put(List(8, 3, 1))
+          else put(List(9, 2, 1))
+   } yield r
+  
   
   def demo = {
     println("============== Stateful ===============")
     
-    for {
-      x <- stackManip(List(5, 8, 2, 1))
-      _ <- show(x)
-    } yield x
+    val t2 = stackManip(List(5, 8, 2, 1))
+    println(s"State: $t2")
+    
+    val tuple2 = stackyStack(List(1, 2, 3))
+    println(s"Current State: $tuple2")
     
     println("")
   }
--- src/main/scala/org/gwgs/learningscalaz/day07/worksheet.sc
+++ src/main/scala/org/gwgs/learningscalaz/day07/worksheet.sc
@@ -8,20 +8,6 @@ object worksheet {
    
   import StatefulComputation._
   
-  stackManip(List(5, 8, 2, 1))                    //> java.lang.IncompatibleClassChangeError: Found class scalaz.IndexedStateT, bu
-                                                  //| t interface was expected
-                                                  //| 	at org.gwgs.learningscalaz.day07.StatefulComputation$.stackManip(Statefu
-                                                  //| lComputation.scala:18)
-                                                  //| 	at org.gwgs.learningscalaz.day07.worksheet$$anonfun$main$1.apply$mcV$sp(
-                                                  //| org.gwgs.learningscalaz.day07.worksheet.scala:11)
-                                                  //| 	at org.scalaide.worksheet.runtime.library.WorksheetSupport$$anonfun$$exe
-                                                  //| cute$1.apply$mcV$sp(WorksheetSupport.scala:76)
-                                                  //| 	at org.scalaide.worksheet.runtime.library.WorksheetSupport$.redirected(W
-                                                  //| orksheetSupport.scala:65)
-                                                  //| 	at org.scalaide.worksheet.runtime.library.WorksheetSupport$.$execute(Wor
-                                                  //| ksheetSupport.scala:75)
-                                                  //| 	at org.gwgs.learningscalaz.day07.worksheet$.main(org.gwgs.learningscalaz
-                                                  //| .day07.worksheet.scala:3)
-                                                  //| 	at org.gwgs.learningscalaz.day07.worksheet.main(org.gwgs.learningscalaz.
-                                                  //| day07.worksheet.scala)
+  stackManip(List(5, 8, 2, 1))                    //> res0: scalaz.Id.Id[(org.gwgs.learningscalaz.day07.StatefulComputation.Stack,
+                                                  //|  Int)] = (List(8, 2, 1),5)
 }
